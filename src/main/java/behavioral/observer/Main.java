c3e7909d46d7e48d78a62368547cda7a296bd2c9
Dhruba Chakraborty <dhrubachakraborty@Mac.bbrouter>
2025-12-10 21:41:32 +0800

    feat(Observer) - Observer Pattern implementation

---
+++ src/main/java/behavioral/observer/Main.java
@@ -0,0 +1,29 @@
+package main.java.behavioral.observer;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+    static void main() throws InterruptedException {
+        Stock apl = new Stock("APL", 212.00d);
+        Observer investor1 = (msg) -> IO.println("Investor:: " + msg);
+        apl.attach(investor1);
+
+        var executorService = Executors.newVirtualThreadPerTaskExecutor();
+
+        executorService.submit(() -> {
+            apl.updatePrice(220.00d);
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            apl.updatePrice(210.00d);
+
+        });
+        executorService.shutdown();
+        executorService.awaitTermination(2, TimeUnit.SECONDS);
+        apl.detach(investor1);
+        apl.updatePrice(200.00d);
+    }
+}
---
+++ src/main/java/behavioral/observer/PriceUpdatable.java
@@ -0,0 +1,5 @@
+package main.java.behavioral.observer;
+
+public interface PriceUpdatable {
+    void updatePrice(double newPrice);
+}
