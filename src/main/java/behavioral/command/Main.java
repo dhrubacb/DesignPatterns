900a6f9b2d0ba8b98f7efe90c4853be32710d224
Dhruba Chakraborty <dhrubachakraborty@Mac.bbrouter>
2025-12-23 21:01:06 +0800

    fix/rename

--- src/main/java/behavioral/command/Main.java
+++ src/main/java/behavioral/command/Main.java
@@ -3,12 +3,12 @@ package main.java.behavioral.command;
 public class Main {
     static void main() {
         TextFile textFile = new TextFile("untitled.txt");
-        Runnable openFile = textFile::open;
-        Runnable saveFile = textFile::save;
-        Runnable closeFile = textFile::close;
-        MenuButton openButton = new MenuButton(openFile);
-        MenuButton saveButton = new MenuButton(saveFile);
-        MenuButton closeButton = new MenuButton(closeFile);
+        Runnable openFileCommand = textFile::open;
+        Runnable saveFileCommand = textFile::save;
+        Runnable closeFileCommand = textFile::close;
+        MenuButton openButton = new MenuButton(openFileCommand);
+        MenuButton saveButton = new MenuButton(saveFileCommand);
+        MenuButton closeButton = new MenuButton(closeFileCommand);
         openButton.click();
         saveButton.click();
         closeButton.click();
